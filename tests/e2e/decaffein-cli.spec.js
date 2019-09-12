c9e55dba2297f788716a339545ffeb736481ac77
seonghuichoe <heeday0410@gmail.com>
2019-09-13 00:09:03 +0900

    Rename decaffein to decaffeine

--- tests/e2e/decaffein-cli.spec.js
+++ tests/e2e/decaffein-cli.spec.js
@@ -2,19 +2,19 @@ const path = require('path')
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-describe('decaffein CLI', () => {
+describe('decaffeine CLI', () => {
   it('command exists', async () => {
     try {
-      const { stderr } = await exec('./bin/decaffein')
+      const { stderr } = await exec('./bin/decaffeine')
       expect(stderr).toBe('')
     } catch (e) {
       expect(e).toBe(null)
     }
   })
 
-  it('decaffein build', async () => {
+  it('decaffeine build', async () => {
     const projectPath = path.resolve('tests', 'test-site')
-    const command = './../../bin/decaffein build'
+    const command = './../../bin/decaffeine build'
 
     const { stdout } = await exec(`cd ${projectPath} && ${command}`)
 
