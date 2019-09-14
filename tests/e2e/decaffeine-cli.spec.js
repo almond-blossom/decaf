72610a4d9a60750f2fe46f1fe7fb822659355e99
edunga1 <goonr21@gmail.com>
2019-09-15 02:46:35 +0900

    test: Update tests that have to be implemented.

--- tests/e2e/decaffeine-cli.spec.js
+++ tests/e2e/decaffeine-cli.spec.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const util = require('util')
+const fs = require('fs')
 const exec = util.promisify(require('child_process').exec)
-const readdir = util.promisify(require('fs').readdir)
+
+const readdir = util.promisify(fs.readdir)
+const readFile = util.promisify(fs.readFile)
 
 describe('decaffeine CLI', () => {
   it('command omitted', async () => {
@@ -10,11 +13,17 @@ describe('decaffeine CLI', () => {
 })
 
 describe('decaffeine build', () => {
-  it('generates distribution', async () => {
+  async function execBuild() {
     const projectPath = path.resolve('tests', 'test-site')
     const command = './../../bin/decaffeine build'
 
     const { stdout } = await exec(`cd ${projectPath} && ${command}`)
+
+    return { stdout, projectPath }
+  }
+
+  it('generates distribution', async () => {
+    const { stdout, projectPath } = await execBuild()
 
     expect(stdout).not.toBeNull()
 
@@ -28,5 +37,18 @@ describe('decaffeine build', () => {
     const pfiles = await readdir(`${projectPath}/dist/posts`)
     expect(pfiles).toContain('post1.html')
     expect(pfiles).toContain('post2.html')
+  })
+
+  it('test index.html', async () => {
+  })
+
+  it('test posts', async () => {
+    const { projectPath } = await execBuild()
+
+    const post1 = await readFile(`${projectPath}/dist/posts/post1.html`, 'utf8')
+    expect(post1).toMatch('테스트 입니다.')
+
+    const post2 = await readFile(`${projectPath}/dist/posts/post2.html`, 'utf8')
+    expect(post2).toMatch('테스트2 입니다.')
   })
 })
