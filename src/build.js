4ea97e0f882ec4bd719e2ebf202d764eaf20a49f
edunga1 <goonr21@gmail.com>
2019-09-30 00:30:00 +0900

    feat: Add 'docsPath' config
    The docsPath refers to a directory contains markdown files.
    added module to parse the configuration file.
    renamed 'decaffein' to 'decaffeine' in the test site.

--- src/build.js
+++ src/build.js
@@ -1,14 +1,17 @@
 const util = require('util')
 const path = require('path')
 const exec = util.promisify(require('child_process').exec)
+const loadConfig = require('./loadConfig')
 
 /**
  * @param dpath {string}
  */
 module.exports = async (dpath) => {
   const gpath = path.resolve(__dirname, '..', 'gatsby')
+  const { docsPath } = await loadConfig(dpath)
+
   await exec(`rm -rf ${gpath}/.cache ${gpath}/public`)
-  await exec(`cp -r ${dpath}/papers/ ${gpath}/src/markdown-pages/`)
+  await exec(`cp -r ${docsPath} ${gpath}/src/markdown-pages/`)
   await exec(`cd ${gpath} && ./node_modules/.bin/gatsby build`)
   await exec(`mv ${gpath}/public/ ${dpath}/dist/`)
 }
---
+++ src/loadConfig.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const util = require('util')
+const readFile = util.promisify(require('fs').readFile)
+
+/**
+ * @param dpath {string}
+ */
+module.exports = async (dpath) => {
+  const configPath = path.resolve(dpath, 'decaffeine.config.json')
+  const config = await readFile(configPath, 'utf8').catch(() => '{}')
+  const merged = {
+    docsPath: 'documents',
+    ...JSON.parse(config),
+  }
+
+  return {
+    docsPath: path.resolve(dpath, merged.docsPath),
+  }
+}
